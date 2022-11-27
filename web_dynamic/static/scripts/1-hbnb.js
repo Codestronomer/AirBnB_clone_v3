879c33bf89709e80a2ecb5efbee451bb82eb02ea
codestronomer <johnrumide6@gmail.com>
2022-11-27 01:21:43 +0100

    [update] complete jquery script

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -6,5 +6,11 @@ $('document').ready(() => {
                 } else {
                         delete amenities[$(this).data('data-id')];
                 }
-        })
-})
+                if (Object.keys(amenities).length > 0) {
+                        $('.amenities > h4').text(Object.values(amenities).join(', '));
+                }
+                else {
+                        $('.amenities > h4').html('&nbsp;');
+                }
+        });
+});
